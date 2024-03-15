cb0f012fee8ae7503bb151fb7c3597aa88fb100d
arezookmn <arezookmn1@gmail.com>
2024-03-15 13:05:14 +0330

    Add extention method for dynamic registeration services

---
+++ WebApplication/DynamicServiceRegisterationExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+
+public static class DynamicServiceRegisterationExtention
+{
+    public static void AddDynamicServices(this IServiceCollection services, Assembly assembly) 
+    { 
+        var serviceTypes = assembly.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract &&
+            t.GetCustomAttributes(typeof(ServiceAttribute), false).Any());
+
+        foreach (var serviceType in serviceTypes)
+        {
+            var serviceInterface = serviceType.GetInterface($"I{serviceType.Name}");
+
+            var serviceAttribute = (ServiceAttribute)serviceType
+                .GetCustomAttributes(typeof(ServiceAttribute), false)
+                .First();
+
+            services.Add(new ServiceDescriptor(serviceInterface , serviceType, serviceAttribute.Lifetime));
+        }
+
+    }
+
+}
--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,7 +1,9 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDynamicServices(Assembly.GetExecutingAssembly());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
